3ca7f44591af61e6ba62a70e36fe27e142278e71
Lucas-ads10 <lucasdssilva13@hotmail.com>
2024-01-26 15:14:50 -0300

    Props e algumas correções

--- Exercicios/Exercício 2 - Criando um Componente Card/src/App.jsx
+++ Exercicios/Exercício 2 - Criando um Componente Card/src/App.jsx
@@ -1,10 +1,15 @@
 import Card from "./components/Card"
+import starWarsPosterImg from "./assets/starWars.jpg"
+import empirePosterImg from "./assets/empire.jpg"
+import returnPosterImg from "./assets/return.jpg"
 
 function App(){
     return(
-        <div className="app">
-            <Card />
-        </div>
+        <>
+            <Card title="Pôstar: Star Wars (1977)" posterImg={starWarsPosterImg} />
+            <Card title="Pôster: Empire Stikes Back (1980)" posterImg={empirePosterImg}/>
+            <Card title="Pôster: Return of the Jedi (1983)" posterImg={returnPosterImg}/>
+        </>
     )
 }
 
