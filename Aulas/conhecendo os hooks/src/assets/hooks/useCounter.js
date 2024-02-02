e459c1065aa2d87c7f960d2c04833aca79f68e59
Lucas-ads10 <lucasdssilva13@hotmail.com>
2024-02-01 21:00:08 -0300

    conhecendo melhor o use state

--- Aulas/conhecendo os hooks/src/assets/hooks/useCounter.js
+++ Aulas/conhecendo os hooks/src/assets/hooks/useCounter.js
@@ -1,10 +1,16 @@
 import { useState } from 'react'
 
+function getInitialValue(){
+    console.log("obtendo um valor inicial")
+    return 1 + 1
+}
+
 export default function useCounter(){
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(() => getInitialValue()) //Não está chamando a função e sim a definbição dela
 
     const increment = () => {
-        setCount(count + 1)
+        setCount ((batata) => batata + 1) //usar as funções incluindo os dois valores
+        setCount ((batata) => batata + 1)
     }
 
     return { count, increment}
